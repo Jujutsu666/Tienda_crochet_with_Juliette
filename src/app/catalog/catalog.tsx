05d9183d17d0b8ae10edcdc95d169e648ac2de8d
jujutsu666 <jr.traduction1@gmail.com>
2023-09-17 19:31:20 -0700

    images, about

---
+++ src/app/catalog/catalog.tsx
@@ -0,0 +1,24 @@
+export default function Catalog() {
+  return (
+    <main className="">
+      <header>
+        <p>
+          here i will put my 2 images. Catalog + crochet with juliette uno sobre
+          el otro
+        </p>
+      </header>
+      <section>
+        <p>
+          here i want to put 8 images. 4 en la primera linea y abajo 4 más. cada
+          imagen tendrá contenido debajo ej: nombre, precio luego estrellas para
+          los testimonios
+        </p>
+      </section>
+      <footer>
+        <p>ahí va el footer blanco </p>
+      </footer>
+    </main>
+  );
+}
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+
+
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <section className="min-h-[calc(100vh-96px)] bg-black text-white w-full flex justify-center items-center text-8xl">Hero</section>
+      <section className="min-h-[calc(100vh-96px)] bg-black text-white w-full flex justify-center items-center text-8xl">FUCK
+      </section>
     </main>
   );
 }
+
