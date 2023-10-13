5279e17e0698f801243e45b7606959231e342cbd
jujutsu666 <jr.traduction1@gmail.com>
2023-10-12 17:08:29 -0700

    icons Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,38 @@
-/*export default function Footer() {
-  return (
-    <footer className="bg-black w-full text-white">
-      <div className=" text-gray-800  flex justify-center items-center  space-x-4 ">
-        <p className="hover:text-white">Author: Juliette Redois-Guillard</p>
-        <a className="hover:text-white" href="mailto:jr.traduction1@gmail.com">
-          crochetwithjuliette@gmail.com
-        </a>
-      
-        <p className=" hover:text-white ">© Copy Right 2023</p>
-      </div>
-    </footer>
-  );
-}*/
+
 
 import Image from "next/image";
 import whatsapp from "public/images/whatsapp.png";
-import instagram from "public/images/instagram.png";
+import insta from "public/images/insta.png";
+import fb from "public/images/fb.png";
+import x from "public/images/x.png";
 export default function Footer() {
   return (
     <footer className="box-border m-0 p-0 bg-[#111]">
       <div className="w-full pt-[70px] pb-5 px-[30px]">
         <div className="flex justify-center ">
           <a
-            className="no-underline bg-[white] m-5 p-2.5 rounded-[50%] transition-[background-color] duration-[0.5s] hover:bg-pink-500 opacity-90"
-            href="https://instagram.com/crochette_with_juliette?igshid=OGQ5ZDc2ODk2ZA=="
+            className="no-underline  m-5 p-1 rounded-[30%] transition-[background-color] duration-[0.5s] hover:bg-[#192a3d] opacity-90"
+            href="#"
           >
-            <Image
-              src={instagram}
-              alt="instagram"
-              className="w-[30px] text-[black] opacity-70 m-0.5"
-            />
+            <Image src={fb} alt="instagram" className="w-[50px]  m-0.5" />
           </a>
           <a
-            className="no-underline bg-[white] m-5 p-2.5 rounded-[50%] transition-[background-color] duration-[0.5s] hover:bg-green-500 opacity-90"
+            className="no-underline  m-5 p-1 rounded-[30%] transition-[background-color] duration-[0.5s] hover:bg-[#d75a8a] opacity-90"
+            href="https://instagram.com/crochette_with_juliette?igshid=OGQ5ZDc2ODk2ZA=="
+          >
+            <Image src={insta} alt="instagram" className="w-[50px]  m-0.5" />
+          </a>
+          <a
+            className="no-underline  m-5 p-1 rounded-[30%] transition-[background-color] duration-[0.5s] hover:bg-[#2d3c1c] opacity-90"
             href="tel:+52613-139-3743"
           >
-            <Image
-              className="w-[30px] text-[black] opacity-70 m-0.5"
-              src={whatsapp}
-              alt="whatsapp"
-            />
+            <Image className="w-[50px] m-0.5" src={whatsapp} alt="whatsapp" />
+          </a>
+          <a
+            className="no-underline  m-5 p-1 rounded-[30%] transition-[background-color] duration-[0.5s] hover:bg-[#252428] opacity-90"
+            href="#"
+          >
+            <Image src={x} alt="instagram" className="w-[50px]  m-0.5" />
           </a>
         </div>
         <div className="mx-0 my-[30px]">
