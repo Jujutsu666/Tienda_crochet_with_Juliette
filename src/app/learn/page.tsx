0e26f06c08770d4426f9af7949f79f9f73ac48ff
jujutsu666 <jr.traduction1@gmail.com>
2023-11-07 12:43:31 -0700

    idk

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,89 +1,403 @@
-import Image from "next/image";
-import myImage from "public/images/learn.png";
-import "./style.css"
+import "./learn.css";
 
 export default function Learn() {
   return (
     <main className="flex flex-col items-center justify-between">
       <section className="min-h-[calc(100vh-96px)]  w-full flex flex-col items-center">
         <div className="pt-[70px] pb-[20px] ">
-          <h1>LEARN TO CROCHET</h1>
+          <h1 className="font-[MANIFESTO]">LEARN TO CROCHET</h1>
         </div>
 
-        <div className="pt-[50px] pb-[50px] text-left">
-          <h2>Introduction to Crochet</h2>
-          <ul>
-            <li>
-              <a href="https://youtu.be/GILITf4wsFQ">Crochet vs knitting</a>
-            </li>
-            <li>
-              <a href="https://youtu.be/zsUbW2d2Bfg">
-                Crochet tools and materials
-              </a>
-            </li>
-            <li>Crochet terminology (US vs. UK terms)</li>
-            <li>Common crochet abbreviations</li>
-            <li>
-              <a href="https://youtu.be/sHfUsoElVDk">Terms you should know</a>
-            </li>
-          </ul>
+        <div className="pt-[50px] pb-[50px] text-left font-[MANIFESTO]">
+          <div>
+            <h2>Introduction to Crochet</h2>
 
-          <h2>Crochet Hooks</h2>
-          <ul>
-            <li>
-              <a href="https://youtu.be/_cXRPUbV6Ro">
-                Types of crochet hooks, sizes and how to read them
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/95uMoNa_5Sw">
-                Selecting the right hook for your yarn and project
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/yC2XpaXo48A">
-                How to hold a crochet hook for comfortable crocheting
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/YvdfdkaL1Qw">
-                Another video on how to hold a crochet hook
-              </a>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                <a href="https://youtu.be/GILITf4wsFQ">Crochet vs Knitting</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/zsUbW2d2Bfg">
+                  Crochet Tools and Materials
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/sHfUsoElVDk">
+                  Crochet Terminology (US vs. UK Terms)
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/sHfUsoElVDk">
+                  Common Crochet Abbreviations
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Hooks</h2>
 
-          <h2>Yarn</h2>
-          <ul>
-            <li>
-              <a href="https://youtu.be/QKryW4HFiY0">Yarn weight categories</a>
-            </li>
-            <li>
-              <a href="https://youtu.be/cRuAe6DkDFg">Yarn fiber types</a>
-            </li>
-            <li>
-              <a href="https://youtu.be/z-wMuHHT7rw">How to read yarn labels</a>
-            </li>
-            <li>
-              <a href="https://youtu.be/SBh1xYOrOFw">
-                Yarn substitutions and color selection
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/5xzjp72afow">
-                Another video on yarn substitutions and color selection
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/yyMwEdE-DtU">
-                Techniques for working with specialty yarns
-              </a>
-            </li>
-            <li>
-              <a href="https://youtu.be/OEjrupJDVXk">
-                Another video on techniques for working with specialty yarns
-              </a>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                <a href="https://youtu.be/_cXRPUbV6Ro">
+                  Types of Crochet Hooks, Sizes and How to Read Them
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/95uMoNa_5Sw">
+                  Selecting the Right Hook for Your Yarn and Project
+                </a>
+              </li>
+              <li>
+                <p>
+                  How to Hold a Crochet Hook for Comfortable Crocheting ....
+                  <a className="watchVideo" href="https://youtu.be/yC2XpaXo48A">
+                    Method 1
+                  </a>
+                  ....
+                  <a className="watchVideo" href="https://youtu.be/YvdfdkaL1Qw">
+                    Method 2
+                  </a>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Yarn</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/QKryW4HFiY0">
+                  Yarn Weight Categories
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/cRuAe6DkDFg">Yarn Fiber Types</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/z-wMuHHT7rw">
+                  How to Read Yarn Labels
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/SBh1xYOrOFw">
+                  Yarn Substitutions and Color Selection
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/5xzjp72afow">
+                  Techniques for Working with Specialty Yarns
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Basic Crochet Stitches</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/zsWmVcp9RMU">Slip Knot</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/7v-mssy5rKU">Chain (ch)</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/vKX4wyTh3pY">Single Crochet (sc)</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/L9bnDlbZj7E">
+                  Half Double Crochet (hdc)
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/fppVmG-oWoE">Double Crochet (dc)</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/5iFGojNjjsY">Treble Crochet (tr)</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/N1jKz_5qQqs">Slip Stitch (sl st)</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/0gHmLDR2QLY">All</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Foundation</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/npUnNdRVyXE">Joining in the Round</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/qUJyZgNChG8">Working in Rows</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Techniques</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/M70YQEFuRtU">Turning Chains</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/JvpTMF2vNNc">
+                  Working in the Front Loop, Back Loop, and Both Loops
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/VUCNC4DTtwo">
+                  Crocheting in a Continuous Spiral
+                </a>
+              </li>
+              <li>
+                <p>
+                  <a className="watchVideo" href="https://youtu.be/YTZpaLROlCA">
+                    Changing Colors
+                  </a>
+                  .and.
+                  <a href="https://www.youtube.com/shorts/pu1kn4CgwTM?feature=share">
+                    Managing Yarn Tails
+                  </a>
+                </p>
+              </li>
+
+              <li>
+                <a href="https://youtu.be/FfH6iMAqzkk">
+                  Working with Multiple Strands of Yarn
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/9DCN4RNO7jg">
+                  Crocheting in the Round and Joining Rounds Seamlessly
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Patterns</h2>
+
+            <ul>
+              <li>
+                <p>
+                  Reading Crochet Patterns and Understanding Gauge and
+                  Swatching...
+                  <a className="watchVideo" href="https://youtu.be/x274USazFs8">
+                    CHARTS and SYMBOLS
+                  </a>
+                  ...
+                  <a className="watchVideo" href="https://youtu.be/rRM5C7C2sFI">
+                    crochet pattern abreviations
+                  </a>
+                </p>
+              </li>
+
+              <li>
+                <a href="https://youtu.be/f8bvcUddGOw">
+                  How to Modify and Customize Crochet Patterns
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/q9YV8glf5bc">
+                  Techniques for Writing Your Own Crochet Patterns
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Projects</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/NaTz0gEzY1E">
+                  How to Plan and Organize a Crochet Project
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Finishing Techniques</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/F0BdEggAF3U">
+                  Fastening Off and Securing the Last Stitch
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/vWypan4dewE">
+                  Weaving in Ends Neatly
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/v0Hfu0byjxs">
+                  Blocking and Shaping Your Crochet Projects
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/_nfrzYOk8Rg">
+                  Techniques for Adding Buttons and Zippers
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Edgings and Borders</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/904wGzBtLwY">
+                  Adding a Simple Border to Projects
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/MQmO74im2N0">Picot Edging</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/3G2YLfyQGvs">Scalloped Edges</a>
+              </li>
+
+              <li>
+                <p>
+                  Design and Execute Custom Edgings...
+                  <a className="watchVideo" href="https://youtu.be/HNe5K8CKzXQ">
+                    Trim Stitch Pattern #857
+                  </a>
+                  ...
+                  <a className="watchVideo" href="https://youtu.be/G66xuM8G2K0">
+                    Trim Stitch Pattern #852
+                  </a>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Stitch Patterns</h2>
+
+            <ul>
+              <li>
+                <p>
+                  Texture Stitches...
+                  <a className="watchVideo" href="https://youtu.be/WJDyp6KoRkU">
+                    Wattle Stitch
+                  </a>
+                  ...
+                  <a className="watchVideo" href="https://youtu.be/pb2miumLREI">
+                    Single Compress Stitch
+                  </a>
+                  ...
+                  <a className="watchVideo" href="https://youtu.be/7J4eIBEa4lY">
+                    Almond Stitch
+                  </a>
+                  ...
+                  <a className="watchVideo" href="https://youtu.be/7TyIsYr58Cs">
+                    Popcorn Stitch
+                  </a>
+                </p>
+              </li>
+
+              <li>
+                <a href="https://youtu.be/Mn6oRH0HSsE">Lace Stitches</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/4-hkjV0VW2E">Filet Crochet</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/LBi3RgBbV2A">Cable Stitches</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet in the Round</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/p298HxgsO1s">
+                  Magic Ring (Magic Circle)
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/hAeuRMAtxCY">
+                  Tips for Keeping Track of Rounds
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Increases and Decreases</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/CvD1qrrRX5c">Increasing Stitches</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/ZXsiVk52_vA">Decreasing Stitches</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/Q8jh3m_xlRY">
+                  Techniques for Shaping in Crochet Projects
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Colorwork</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/De1vILCzOeE">Intarsia Crochet</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/NQwvHs9vxkA">Fair Isle Crochet</a>
+              </li>
+              <li>
+                <a href="https://youtu.be/L1GYasCHJEc">
+                  Tapestry Crochet and Color Changes
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/zbt5bZThDfs">
+                  How to Read Colorwork Charts
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Crochet Joining Techniques</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/M8SkpizU_0E">
+                  Whipstitch Join for Seams
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/Ul3AUXDIU6I">
+                  Join-as-You-Go (JAYG) for Motifs and Granny Squares
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/M8SkpizU_0E">
+                  Techniques for Aligning and Joining Pieces
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Troubleshooting and Common Mistakes</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/wT2qeW0qEsM">
+                  Fixing Dropped Stitches and Correcting Errors
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/H6FuTHAxKAQ">
+                  Adjusting Tension for Even Crochet Fabric
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/TPWMCpybmLk">
+                  Common Mistakes and How to Avoid Them
+                </a>
+              </li>
+              <li>
+                <a href="https://youtu.be/pOA24_RC8yA">
+                  Strategies for Frogging (Unraveling) a Project
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </main>
